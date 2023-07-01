49b0ebf934d5a5ec6d03b176059314a74e498416
Kresna <krocoach@gmail.com>
2023-07-02 01:03:53 +0700

    feat: add feature to get All Users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,29 @@
-
+const { User, Match } = require('../models')
 
 
 class UserController {
     static async registerUser(req, res, next) {
         try {
             
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static async getAllUsers(req, res, next) {
+        try {
+            const users = await User.findAll({
+                attributes: {
+                    exclude: ['createdAt', 'updatedAt']
+                },
+                include: [{
+                    model: Match,
+                    attributes: {
+                        exclude: ['createdAt', 'updatedAt']
+                    },
+                }]
+            })
+            res.status(200).json(users)
         } catch (error) {
             console.log(error)
         }
--- server/routers/user.js
+++ server/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const UserController = require('../controllers/userController')
 const router = express.Router()
 
+router.get('/', UserController.getAllUsers)
 router.post('/register', UserController.registerUser)
 
 
