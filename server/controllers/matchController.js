8f1b78f61aeec0e85b00a95b0e2ff86e663ff8ed
Kresna <krocoach@gmail.com>
2023-07-02 10:58:47 +0700

    feat: add new feature for User to follow another User
    also change path name of /user => /users and /matchs => /matches

--- server/controllers/matchController.js
+++ server/controllers/matchController.js
@@ -1,7 +1,15 @@
+const { Match } = require('../models')
+
+
 class MatchController {
-    static async followUser() {
+    static async followUser(req, res, next) {
+        const followingId = +req.body.followingId
+        const followerId = +req.body.followerId
+        console.log(followingId, followerId)
         try {
-            
+            const followUser = await Match.create({followingId, followerId})
+
+            res.status(200).json(followUser)
         } catch (error) {
             console.log(error)
         }
--- server/routers/index.js
+++ server/routers/index.js
@@ -3,8 +3,8 @@ const user = require('./user')
 const match = require('./match')
 const router = express.Router()
 
-router.use('/user', user)
-router.use('/matchs', match)
+router.use('/users', user)
+router.use('/matches', match)
 
 
 module.exports = router
